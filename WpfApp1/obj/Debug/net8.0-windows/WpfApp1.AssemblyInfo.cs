bef7ead3e9bf9bf3407d8ed5ba9fd1920d431035
sanjanadissa <sddsdonz@gmail.com>
2025-03-29 00:07:57 +0530

    add assets wddsdaxx

--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf81b4f2b9cefffffcfa9651fcc88f65d86deeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+359f9c7c87961d2a15d2daa7ca9834edcdda43d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
